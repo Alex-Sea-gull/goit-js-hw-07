86382f3718fe98b153b4df6018b2bde4521336e5
Alex Sea <seagullalex81@gmail.com>
2024-07-01 13:28:52 +0300

    add task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,59 +1,17 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
+const quantityLiItem = document.querySelectorAll(".form-item")
+console.log(`Number of categories: ${quantityLiItem.length}`);
 
-  getBalance() {
-    // console.log(`Balance: ${this.balance}`);
-    return this.balance;
-  },
-  getDiscount() {
-    // console.log(`Discount: ${this.discount}`);
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-    // console.log(`Value: ${value}`);
-  },
-  getOrders() {
-    // console.log(`Orders: ${this.orders}`);
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-    // console.log(`Order: ${order} and ${this.balance}`);
-  },
+quantityLiItem.forEach(category => {
+    const title = category.querySelector('.form-titles').textContent;
+    const items = category.querySelectorAll('.form-list-item');
 
-};
+    console.log(`category: ${title}`);
+    console.log(`Elements: ${items.length}`);
+})
 
 
+// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
+// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
-// Використай цей стартовий код і виконай рефакторинг.Після оголошення об'єкта ми додали виклики методів. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
-
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
-
-
-
-// На що буде звертати увагу ментор при перевірці:
-
-// Оголошена змінна customer
-// Значення змінної customer — це об'єкт із властивостями та методами
-// Виклик customer.getDiscount() повертає поточне значення властивості discount
-// Виклик customer.setDiscount(0.15) оновлює значення властивості discount
-// Виклик customer.getBalance() повертає поточне значення властивості balance.
-// Виклик customer.getOrders() повертає поточне значення властивості orders
-// Виклик customer.addOrder(5000, "Steak") додає "Steak" у масив значень властивості orders та оновлює баланс
-// Метод getBalance об'єкта customer використовує this
-// Метод getDiscount об'єкта customer використовує this
-// Метод setDiscount об'єкта customer використовує this
-// Метод getOrders об'єкта customer використовує this
-// Метод addOrder об'єкта customer використовує this
