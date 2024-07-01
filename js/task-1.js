7a65a88e8852aa057b17dbf06369f9661fd87856
Alex Sea <seagullalex81@gmail.com>
2024-07-01 15:48:02 +0300

    add task - 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,17 @@
-const quantityLiItem = document.querySelectorAll(".form-item")
-console.log(`Number of categories: ${quantityLiItem.length}`);
+// const quantityLiItem = document.querySelectorAll(".form-item")
+// console.log(`Number of categories: ${quantityLiItem.length}`);
 
-quantityLiItem.forEach(category => {
-    const title = category.querySelector('.form-titles').textContent;
-    const items = category.querySelectorAll('.form-list-item');
+// quantityLiItem.forEach(category => {
+//     const title = category.querySelector('.form-titles').textContent;
+//     const items = category.querySelectorAll('.form-list-item');
 
-    console.log(`category: ${title}`);
-    console.log(`Elements: ${items.length}`);
-})
+//     console.log(`category: ${title}`);
+//     console.log(`Elements: ${items.length}`);
+// })
 
 
-// З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
+// // З використанням властивостей і методів DOM-елементів, напиши скрипт, який:
 
-// Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-// Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
+// // Порахує й виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// // Для кожного елемента li.item у списку ul#categories знайде й виведе в консоль текст заголовка елемента (тегу <h2>) і кількість елементів у категорії (усіх <li>, вкладених у нього).
 
